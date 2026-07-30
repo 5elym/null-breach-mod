4c0af1dc5221be76f6c64314857de02d3641cf78
Myles Swamy <mswamy2@sheffield.ac.uk>
2026-07-30 22:10:22 +0100

    initial commit

---
+++ src/client/java/com/notelyis/nullstep/client/NullStepClient.java
@@ -0,0 +1,10 @@
+package com.notelyis.nullstep.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NullStepClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
